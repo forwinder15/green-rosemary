a9ced288e6c8eca48019ee728260780d928fd2c3
forwinder15 <jrogercoker@gmail.com>
2020-08-20 22:23:49 -0400

    sermons

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,6 @@ export default class Header extends React.Component {
                       ))}
                       <li className={'menu-item ' + ' current-menu-item' + 'pageContext.url' + "url"} key="4" > <Link to="https://www.paypal.com/donate/?token=KP8kKup9LZ6BST-ynAKb3o6U9HAfS35kGOXggFcomiMe_bgg1g3av-h0Y2Fj5q_pR-Qw10&country.x=US&locale.x=US">Give</Link></li>
 
-                      <li className={'menu-item ' + ' current-menu-item' + 'pageContext.url' + "url"} key="3" > <Link to="/sermons">Recent Sermons</Link></li>
                     </ul>
                     {_.get(this.props, 'pageContext.site.siteMetadata.header.has_social') &&
                       <Social {...this.props} />
