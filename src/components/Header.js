54b898e3a33c874ce55e4a36a993d1bb2fe8bb5c
forwinder15 <jrogercoker@gmail.com>
2020-08-20 21:05:55 -0400

    sermons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,121 +1,61 @@
 import React from 'react';
 import _ from 'lodash';
 
-import {
-  Link,
-  safePrefix
-} from '../utils';
+import { Link, safePrefix } from '../utils';
 import Social from './Social';
 
 export default class Header extends React.Component {
   render() {
-    return ( <
-      header id = "masthead"
-      className = {
-        'site-header ' + _.get(this.props, 'pageContext.site.siteMetadata.header.bg')
-      } >
-      <
-      div className = "site-header-wrap" >
-      <
-      div className = "site-header-inside" >
-      <
-      div className = "site-branding" > {
-        _.get(this.props, 'pageContext.site.siteMetadata.header.profile_img') &&
-        <
-        p className = "profile" >
-        <
-        Link to = {
-          safePrefix('/')
-        } > < img src = {
-          safePrefix(_.get(this.props, 'pageContext.site.siteMetadata.header.profile_img'))
-        }
-        className = "avatar"
-        alt = "Author Avatar" / > < /Link> <
-        /p>
-      } <
-      div className = "site-identity" > {
-        ((_.get(this.props, 'pageContext.frontmatter.template') === 'home') || (_.get(this.props, 'pageContext.frontmatter.template') === 'blog')) ?
-        <
-        h1 className = "site-title" > < Link to = {
-          safePrefix('/')
-        } > {
-          _.get(this.props, 'pageContext.site.siteMetadata.header.title')
-        } < /Link></h1 >
-        :
-          <
-          p className = "site-title" > < Link to = {
-            safePrefix('/')
-          } > {
-            _.get(this.props, 'pageContext.site.siteMetadata.header.title')
-          } < /Link></p >
-      } {
-        _.get(this.props, 'pageContext.site.siteMetadata.header.tagline') &&
-          <
-          p className = "site-description" > {
-            _.get(this.props, 'pageContext.site.siteMetadata.header.tagline')
-          } < /p>
-      } <
-      /div> {
-        (_.get(this.props, 'pageContext.menus.main') && _.get(this.props, 'pageContext.site.siteMetadata.header.has_nav')) &&
-        <
-        button id = "menu-toggle"
-        className = "menu-toggle" > < span className = "screen-reader-text" > Menu < /span><span className="icon-menu"
-        aria - hidden = "true" / > < /button>
-      } <
-      /div> {
-        (_.get(this.props, 'pageContext.menus.main') && _.get(this.props, 'pageContext.site.siteMetadata.header.has_nav')) &&
-        <
-        nav id = "main-navigation"
-        className = "site-navigation"
-        aria - label = "Main Navigation" >
-          <
-          div className = "site-nav-wrap" >
-          <
-          div className = "site-nav-inside" >
-          <
-          ul className = "menu" > {
-            _.map(_.get(this.props, 'pageContext.menus.main'), (item, item_idx) => ( <
-              li key = {
-                item_idx
+    return (
+      <header id="masthead" className={'site-header ' + _.get(this.props, 'pageContext.site.siteMetadata.header.bg')}>
+        <div className="site-header-wrap">
+          <div className="site-header-inside">
+            <div className="site-branding">
+              {_.get(this.props, 'pageContext.site.siteMetadata.header.profile_img') &&
+                <p className="profile">
+                  <Link to={safePrefix('/')}><img src={safePrefix(_.get(this.props, 'pageContext.site.siteMetadata.header.profile_img'))}
+                    className="avatar" alt="Author Avatar" /></Link>
+                </p>
               }
-              className = {
-                'menu-item ' + ((_.get(this.props, 'pageContext.url') === _.get(item, 'url')) ? ' current-menu-item' : '')
-              } >
-              <
-              Link to = {
-                safePrefix(_.get(item, 'url'))
-              } > {
-                _.get(item, 'title')
-              } < /Link> <
-              /li>
-            ))
-          } <
-          li className = {
-            'menu-item ' + ' current-menu-item' + 'pageContext.url' + "url"
-          }
-        key = "4" > < Link to = "https://www.paypal.com/donate/?token=KP8kKup9LZ6BST-ynAKb3o6U9HAfS35kGOXggFcomiMe_bgg1g3av-h0Y2Fj5q_pR-Qw10&country.x=US&locale.x=US" > Give < /Link></li >
+              <div className="site-identity">
+                {((_.get(this.props, 'pageContext.frontmatter.template') === 'home') || (_.get(this.props, 'pageContext.frontmatter.template') === 'blog')) ?
+                  <h1 className="site-title"><Link to={safePrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.header.title')}</Link></h1>
+                  :
+                  <p className="site-title"><Link to={safePrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.header.title')}</Link></p>
+                }
+                {_.get(this.props, 'pageContext.site.siteMetadata.header.tagline') &&
+                  <p className="site-description">{_.get(this.props, 'pageContext.site.siteMetadata.header.tagline')}</p>
+                }
+              </div>
+              {(_.get(this.props, 'pageContext.menus.main') && _.get(this.props, 'pageContext.site.siteMetadata.header.has_nav')) &&
+                <button id="menu-toggle" className="menu-toggle"><span className="screen-reader-text">Menu</span><span className="icon-menu"
+                  aria-hidden="true" /></button>
+              }
+            </div>
+            {(_.get(this.props, 'pageContext.menus.main') && _.get(this.props, 'pageContext.site.siteMetadata.header.has_nav')) &&
+              <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation">
+                <div className="site-nav-wrap">
+                  <div className="site-nav-inside">
+                    <ul className="menu">
+                      {_.map(_.get(this.props, 'pageContext.menus.main'), (item, item_idx) => (
+                        <li key={item_idx} className={'menu-item ' + ((_.get(this.props, 'pageContext.url') === _.get(item, 'url')) ? ' current-menu-item' : '')}>
+                          <Link to={safePrefix(_.get(item, 'url'))}>{_.get(item, 'title')}</Link>
+                        </li>
+                      ))}
+                      <li className={'menu-item ' + ' current-menu-item' + 'pageContext.url' + "url"} key="4" > <Link to="https://www.paypal.com/donate/?token=KP8kKup9LZ6BST-ynAKb3o6U9HAfS35kGOXggFcomiMe_bgg1g3av-h0Y2Fj5q_pR-Qw10&country.x=US&locale.x=US">Give</Link></li>
 
-          <
-          li className = {
-            'menu-item ' + ' current-menu-item' + 'pageContext.url' + "url"
-          }
-        key = "3" > < Link to = "/sermons" > Recent Sermons < /Link></li >
-          <
-          /ul> {
-            _.get(this.props, 'pageContext.site.siteMetadata.header.has_social') &&
-              <
-              Social {
-                ...this.props
-              }
-            />
-          } <
-          /div> <
-          /div> <
-          /nav>
-      } <
-      /div> <
-      /div> <
-      /header>
+                      <li className={'menu-item ' + ' current-menu-item' + 'pageContext.url' + "url"} key="3" > <Link to="/sermons">Recent Sermons</Link></li>
+                    </ul>
+                    {_.get(this.props, 'pageContext.site.siteMetadata.header.has_social') &&
+                      <Social {...this.props} />
+                    }
+                  </div>
+                </div>
+              </nav>
+            }
+          </div>
+        </div>
+      </header>
     );
   }
 }
