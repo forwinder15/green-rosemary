ed6a8d4aab0e45e4c9f710b57a034443cf9e9e48
forwinder15 <jrogercoker@gmail.com>
2020-08-20 22:51:09 -0400

    sermons

--- src/components/HeroBlock.js
+++ src/components/HeroBlock.js
@@ -15,12 +15,6 @@ export default class HeroBlock extends React.Component {
         <div className="hero-text">
           {markdownify(_.get(this.props, 'section.content'))}
         </div>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-          <input type="hidden" name="cmd" value="_s-xclick" />
-          <input type="hidden" name="hosted_button_id" value="XPRFA5BYMRZP2" />
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-          <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-        </form>
       </section>
     );
   }
