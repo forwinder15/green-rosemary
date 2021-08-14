ddfd5946a35798973f1cffe53797f1f99cd89759
jrogercoker@gmail.com <jrogercoker@gmail.com>
2021-08-14 10:09:52 -0400

    added google

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,15 @@ module.exports = {
   siteMetadata: require("./site-metadata.json"),
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-KF7MJK29E2",// Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
     `gatsby-source-data`,
     {
       resolve: `gatsby-source-filesystem`,
