01ef71f615b4e8db77adf8454bb452ffc50bcd70
John Coker <jrogercoker@gmail.com>
2021-08-25 22:59:23 -0400

    lets see

--- src/components/HeroBlock.js
+++ src/components/HeroBlock.js
@@ -9,8 +9,6 @@ export default class HeroBlock extends React.Component {
       <section id={_.get(this.props, 'section.section_id')} className="hero">
         {_.get(this.props, 'section.title') ?
           <h2 className="hero-title">{_.get(this.props, 'section.title')}</h2>
-          :
-          <h2 className="hero-title">Hi, I'm {_.get(this.props, 'pageContext.site.data.author.name')}.</h2>
         }
         <div className="hero-text">
           {markdownify(_.get(this.props, 'section.content'))}
